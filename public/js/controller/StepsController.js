10c0c23b5e59128878ccbdb61a442f14b44e331b
marabesi <matheus.marabesi@gmail.com>
2015-10-31 18:43:02 -0200

    added validation and corrected some labels

--- public/js/controller/StepsController.js
+++ public/js/controller/StepsController.js
@@ -3,6 +3,41 @@ app.controller('StepsController', ['$scope', function($scope) {
         {}
     ];
     
+    $scope.flow = {
+        type : 'basic'
+    }
+    
+    $scope.complementaries = [];
+    $scope.rules = [];
+    $scope.references = [];
+    
+    $scope.createComplementary = function() {
+        var complementary = {
+            description: $scope.complementary.description
+        }
+        $scope.complementaries.push(complementary);
+        
+        $scope.complementary = null;
+    }
+    
+    $scope.createRule = function() {
+        var rule = {
+            description: $scope.rule.description
+        }
+        $scope.rules.push(rule);
+        
+        $scope.rule = null;
+    }
+    
+    $scope.createReference = function() {
+        var reference = {
+            description: $scope.reference.description
+        }
+        $scope.references.push(reference);
+        
+        $scope.reference = null;
+    }
+    
     $scope.createNewStep = function() {
         var step = {};
         
--- public/js/controller/VersionController.js
+++ public/js/controller/VersionController.js
@@ -1,3 +1,21 @@
-app.controller('VersionController', function() {
+app.controller('VersionController', ['$scope', function($scope) {
+    $scope.versions = [
+        {versionName: '1.0.1', description: 'test'}
+    ]
     
-});
+    $scope.versions = [
+        {versionName: '1.0.0', description: 'test'},
+        {versionName: '2.0.0', description: 'test2'}
+    ]
+    
+    $scope.create = function() {
+        var version = {
+            versionName : $scope.version.name,
+            description : $scope.version.description
+        };
+        
+        $scope.versions.push(version);
+        $scope.version = null;
+        $scope.submitted = true;
+    }
+}]);
